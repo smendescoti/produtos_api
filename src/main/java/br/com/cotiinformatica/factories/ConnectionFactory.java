0774c4ab62aa273a0c2a3bdb53ea1a3446af99c1
smendescoti <sergio.coti@gmail.com>
2022-11-28 11:15:36 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/factories/ConnectionFactory.java
@@ -0,0 +1,17 @@
+package br.com.cotiinformatica.factories;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String HOST = "jdbc:mysql://localhost:3306/bd_apiprodutos?serverTimezone=UTC";
+	private static final String USER = "root";
+	private static final String PASS = "coti";
+
+	public static Connection createConnection() throws Exception {
+		Class.forName(DRIVER);
+		return DriverManager.getConnection(HOST, USER, PASS);
+	}
+}
